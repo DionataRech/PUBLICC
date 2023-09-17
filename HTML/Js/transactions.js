1fc34c196bae56a96f541b15d99d8d8b245fe0b1
DionataRech <dionata.rech@gmail.com>
2023-09-17 18:34:32 -0300

    first commit

---
+++ HTML/Js/transactions.js
@@ -0,0 +1,110 @@
+const myModal = new bootstrap.Modal("#transaction-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+let data = {
+    transactions: []
+};
+
+document.getElementById("buttom-logout").addEventListener("click", logout);
+
+//ADICIONAR LANÇAMENTO
+document.getElementById("transaction-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    const currentBalance = calculateBalance();
+
+    
+    if (type === "2" && value > currentBalance) {
+        if (!confirm("Atenção: O saldo ficará negativo após adicionar esta despesa. Deseja continuar?")) {
+            return;
+        }
+    }
+
+    data.transactions.unshift({
+        value: value, type: type, description: description, date: date
+    });
+
+    saveData(data);
+    e.target.reset();
+    myModal.hide();
+
+    getTransactions();
+
+    alert("Lançamento adicionado com sucesso.");
+});
+
+checkLogged();
+
+function calculateBalance() {
+    const transactions = data.transactions;
+    let balance = 0;
+    transactions.forEach((item) => {
+        if (item.type === "1") {
+            balance += item.value;
+        } else {
+            balance -= item.value;
+        }
+    });
+    return balance;
+}
+
+function checkLogged() {
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+    if(!logged) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser) {
+        data = JSON.parse(dataUser);
+    }
+
+    getTransactions();
+}
+
+function logout(){
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html"
+}
+
+function getTransactions(){
+    const transactions = data.transactions;
+    let transactionsHtml = ``;
+
+    if(transactions.length) {
+        transactions.forEach((item) => {
+            let type = "Entrada";
+
+            if(item.type === "2") {
+                type = "Saída";
+            }
+
+            transactionsHtml += `
+            <tr>
+            <th scope="row">${item.date}</th>
+            <td>${item.value.toFixed(2)}</td>
+            <td>${type}</td>
+            <td>${item.description}</td>
+          </tr>
+          `
+        })
+    }
+
+    document.getElementById("transactions-list").innerHTML = transactionsHtml;
+};
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
